4bfb81272b9404d7c28f4e0d0de28d8d302c487f
Konrad Plocharski <konrad.plocharski@atos.net>
2024-03-09 17:37:07 +0100

    add leave types

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfJoin { get; set; }
+    }
+}
