2c7aaf2fe6cf5f88354f1f51aa4261bb083771bf
Konrad Plocharski <konrad.plocharski@atos.net>
2024-03-10 11:44:13 +0100

    CRUD for leave types

--- LeaveManagement.Web/Data/BaseEntity.cs
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
-        public DateTime DateOfJoin { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
     }
 }
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Default number of days")]
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
